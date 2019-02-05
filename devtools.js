2b0817ffdf6d9b6e0ca0974c15e14e5e7fb0b057
vdeturckheim <vlad2t@hotmail.com>
2019-02-05 17:19:23 +0100

    startup

--- devtools.js
+++ devtools.js
@@ -1,7 +1,5 @@
 'use strict';
 // Create a new panel
-
-
 chrome.devtools.panels.create('SPAudit',
     null,
     'panel.html',
@@ -10,14 +8,16 @@ chrome.devtools.panels.create('SPAudit',
         const cdt = new ChromeDebuggerDriver();
         await cdt.start();
 
+
         panel.onShown.addListener((panelWindow) => {
 
-            panelWindow.document.addEventListener(InstructionEvent.TYPE, (instruction) => {
+            panelWindow.document.addEventListener(InstructionEvent.TYPE, async (instruction) => {
 
                 const { data: { command, params } } = instruction;
-                console.log(command, params);
-                instruction.resolve('ok');
+
+                cdt.sendCommand(command, params)
+                    .then(instruction.resolve)
+                    .catch(instruction.reject);
             });
         });
-    }
-);
+    });
--- panel.js
+++ panel.js
@@ -1,10 +1,26 @@
-function act() {
+const wait = function (t) {
+    return new Promise((r) => {
+        setTimeout(r, t);
+    });
+};
+
+async function act() {
 
     console.log('CLICK');
-    const evt = new InstructionEvent('msg', { ok: 1 });
-    evt.promise.then((x) => console.log('RESOLVE', x));
-
-    document.dispatchEvent(evt);
+    const re = await ChromeDebug.DOM.enable();
+    console.log(re);
+    const re2 = await ChromeDebug.DOM.getDocument();
+    console.log(re2.root.nodeId);
+    const allNodes = await ChromeDebug.DOM.querySelectorAll(re2.nodeId);
+    console.log(allNodes);
+    for (const nodeId of allNodes.nodeIds) {
+        const { object: { objectId } } = await ChromeDebug.DOM.resolveNode(nodeId); // should always be an html element
+        console.log({ nodeId, objectId });
+        const eventListeners = await ChromeDebug.DOMDebugger.getEventListeners(objectId);
+        console.log(eventListeners);
+        // await ChromeDebug.DOM.highlightNode(id);
+        // await wait(1000);
+    }
 }
 
 document.getElementById('btn')
